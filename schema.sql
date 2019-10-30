05a6f972701ecd66d6a335d5b5b99946e53c96e1
Tyler Trinh <tylervtrinh@gmail.com>
2019-10-30 14:26:45 -0700

    Testing changes with Kevin.

--- schema.sql
+++ schema.sql
@@ -4,11 +4,5 @@ CREATE TABLE users(
     password TEXT
 );
 
-
-INSERT INTO tokis (
-    name, height, weight, fly, fight, fire, 
-    water, electric, ice, total, trainer_name)
-    VALUES ('pikachu',130,10,0,12,0,0,100,10,132,'ash ketchum');
-
 INSERT INTO users(
     username, password) VALUES ('ttrinh', 'tst');
