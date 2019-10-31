7397338e618d99e6129529f95baa2ef7af423647
Trevor Grabham <tgrabham@sfu.ca>
2019-10-31 01:54:06 -0700

    added sessions and updated the sign up page with validation checking, just missing hashing and database integration

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+const {check, validationResult} = require('express-validator');
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
 const PORT = process.env.PORT || 5000
 var app = express();
 
@@ -13,49 +16,77 @@ pool = new Pool({
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(expressSession({secret: 'tuinng', saveUninitialized: false, resave: false}));
+app.use(cookieParser());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    // Used to identify the current page to display login or username
-    // Once sessions are setup, used that variable instead to check if they are logged in
-    res.render('pages/index', {title: 'home'});
+    if(req.session.username){
+      res.redirect('play');
+    }
+    else{
+      res.render('pages/index', {title: 'home'});
+    }
 });
 
-app.get('/login', (req, res) => {res.render('pages/login')});
+app.get('/login', (req, res) => {res.render('pages/login', {errors: null})});
 
-app.get('/signup', (req,res) => {res.render('pages/signup')});
+app.get('/signup', (req,res) => {res.render('pages/signup', {errors: null})});
 
 app.post('/sign_in', (req, res) => {
-    var username = req.body.username;
-    var password = req.body.password;
+  var username = req.body.username;
+  var password = req.body.password;
+  var errors = null;
+  // hash
+  // validate on db
 
-    // Validate here
-
-    res.send(username + ' is logged in!');
-
+  if(errors){
+    res.render('pages/login', {errors: errors});
+  }else{
+    req.session.username = username;
+    res.redirect('play');
+  }
 });
 
-app.post('/sign_up', (req, res) => {
-    var username = req.body.username;
-    // query db and check if the username is already in username
-    //  if username is already in use, send error message and re-render page
-    //  else
-    //    var password = req.body.password;
-    //    if(password != req.body.confirmPassword)
-    //      send error message and re-render page (preferably with the same username)
+app.post('/sign_up', [check('password','password is too short').isLength({ min: 5 }), check('username','username is too short').isLength({min:5})], (req, res) => {
+  var username = req.body.username;
+  // query db and check if the username is already in username
+  //  if username is already in use, send error message and re-render page
+  //  else
+  var errors = validationResult(req);
+  if(!(req.body.password === req.body.confirmPassword)){
+    res.render('pages/signup', {errors: [{msg:'Passwords do not match'}]});
+  }
+  else if(!errors.isEmpty()){
+    res.render('pages/signup', errors)
+  }else{
+    //    hash
     //    insert into database values (username, password)
-    //    render homepage
+    req.session.username = username;
+    res.redirect('play');
+  }
 });
 
 app.get('/play', (req, res) => {
-    // Used to identify the current page to display login or username
-    // Once sessions are setup, used that variable instead to check if they are logged in
-    res.render('pages/landing', {title: 'play'});
+  if(req.session.username){
+    res.render('pages/landing', {username: req.session.username, title: 'play'});
+  }else{
+    res.redirect('login');
+  }
+});
+
+app.get('/logout', (req, res) => {
+  if(req.session.username){
+    req.session.destroy((err) =>{});
+    res.redirect('/');
+  }else{
+    res.redirect('login');
+  }
 });
 
 app.get('*', function(req, res){
     res.status(404).send('ERROR 404: The page you requested is invalid or is missing, please try something else')
   })
-  
+
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
