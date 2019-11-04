cb479b58d87bb39526b1b1476fbb183ad1a7fc87
Tyler Trinh <tylervtrinh@gmail.com>
2019-11-03 23:03:24 -0800

    Fix sign up query

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 
 var bcrypt = require('bcrypt');
 const saltRounds = 10;
-//const db = require('./db');
+// const db = require('./db');
 // for when we create database
 const { Pool } = require('pg');
 var pool;
@@ -44,7 +44,7 @@ app.post('/sign_in', (req, res) => {
   var errors = null;
   // hash
   // validate on db
-  db.query('SELECT password FROM users WHERE username = ?',[username], function(err,results,fields) {
+  pool.query('SELECT password FROM users WHERE username = ?',[username], function(err,results,fields) {
     if (error) throw error;
 
     //if (results.length == 0){
@@ -94,10 +94,14 @@ app.post('/sign_up', [check('password','password is too short').isLength({ min: 
     //    dont know db name yet, so swap out users with db name
     bcrypt.hash(password, saltRounds, function(err, hash) {
 
-      db.query('INSERT INTO users (username, password) VALUES (?,?)',(username,hash)), function(error,results, fields){
-        if (error) throw error;
-      }
+      pool.query(`INSERT INTO users (username, password) VALUES ('${username}', '${hash}')`, (error) => {
+        if (error) {
+          throw error;
+        }
+      });
+
     });
+
     req.session.username = username;
     res.redirect('play');
   }
