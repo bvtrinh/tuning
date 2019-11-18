702cc242c3678d5d14b27a77d546c84c2e829c90
Andy Liu <32084972+AndyLiuCodes@users.noreply.github.com>
2019-11-17 20:31:20 -0800

    Update index.js

--- index.js
+++ index.js
@@ -524,7 +524,7 @@ function getRelatedSongs(playlist, callback) {
       }
 
       relatedSongsPool = relatedSongsPool.filter(function (song) {
-        return ((song.id != returnPlaylist[counter-1].songid) || !(song.name.includes(returnPlaylist[counter-1].songname)))
+        return ((song.id != returnPlaylist[counter-1].songid) && !(song.name.includes(returnPlaylist[counter-1].songname)))
       })
 
       // randomly select 3 of those top tracks
